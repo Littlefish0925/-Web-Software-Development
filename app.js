347c9a3da1d8d45d0505bd33abcfb9097f380b1c
Hanze Yu <115174759+Littlefish0925@users.noreply.github.com>
2024-12-12 00:48:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import { Eta } from 'https://deno.land/x/eta@v3.4.0/src/index.ts';
 import { Hono } from 'https://deno.land/x/hono@v3.12.11/mod.ts';
 import { showFormAndSongs, addNewSong } from './songController.js';
 
-const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
+export const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
 
 const app = new Hono();
 
